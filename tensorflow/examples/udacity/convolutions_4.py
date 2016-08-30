e631c563951538ad5ba8597aca820f7f5fdeefbb
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-08-30 18:18:40 +0200

    Much more flexible framework: graph definition, training and testing are in separate functions.

--- tensorflow/examples/udacity/convolutions_4.py
+++ tensorflow/examples/udacity/convolutions_4.py
@@ -2,39 +2,37 @@
 # vim: set fileencoding=utf-8 :
 
 """Exercises for assignment 4 -- convolutions."""
+from collections import namedtuple
 import tensorflow as tf
 
 from common import accuracy, load_data, reformat_conv
 
-def exercise_0(data, image_size=28, num_labels=10, num_channels=1):
-    batch_size = 16
-    patch_size = 5
-    depth = 16
-    num_hidden = 64
 
-    graph = tf.Graph()
-
+def create_graph_0(graph, params):
     with graph.as_default():
         # Input data.
         tf_dataset = tf.placeholder(
-            tf.float32, shape=(None, image_size, image_size, num_channels))
-        tf_labels = tf.placeholder(tf.float32, shape=(None, num_labels))
-        tf_valid_dataset = tf.constant(data['validd'])
-        tf_test_dataset = tf.constant(data['testd'])
+            tf.float32, shape=(None, params.image_size, params.image_size, params.num_channels),
+            name='tf_dataset')
+        tf_labels = tf.placeholder(
+            tf.float32, shape=(None, params.num_labels), name='tf_labels')
 
         # Variables.
         layer1_weights = tf.Variable(tf.truncated_normal(
-            [patch_size, patch_size, num_channels, depth], stddev=0.1))
-        layer1_biases = tf.Variable(tf.zeros([depth]))
+            [params.patch_size, params.patch_size,
+             params.num_channels, params.depth], stddev=0.1))
+        layer1_biases = tf.Variable(tf.zeros([params.depth]))
         layer2_weights = tf.Variable(tf.truncated_normal(
-            [patch_size, patch_size, depth, depth], stddev=0.1))
-        layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+            [params.patch_size, params.patch_size, params.depth, params.depth],
+            stddev=0.1))
+        layer2_biases = tf.Variable(tf.constant(1.0, shape=[params.depth]))
         layer3_weights = tf.Variable(tf.truncated_normal(
-            [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
-        layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+            [params.image_size // 4 * params.image_size // 4 * params.depth,
+             params.num_hidden], stddev=0.1))
+        layer3_biases = tf.Variable(tf.constant(1.0, shape=[params.num_hidden]))
         layer4_weights = tf.Variable(tf.truncated_normal(
-            [num_hidden, num_labels], stddev=0.1))
-        layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+            [params.num_hidden, params.num_labels], stddev=0.1))
+        layer4_biases = tf.Variable(tf.constant(1.0, shape=[params.num_labels]))
 
         # Model.
         def model(data):
@@ -49,40 +47,54 @@ def exercise_0(data, image_size=28, num_labels=10, num_channels=1):
 
         # Training computation.
         logits = model(tf_dataset)
-        loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(logits, tf_labels))
-
-        # Optimizer.
-        optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+        loss = tf.reduce_mean(  # noqa
+            tf.nn.softmax_cross_entropy_with_logits(logits, tf_labels),
+            name='loss')
 
         # Predictions for the training, validation, and test data.
-        prediction = tf.nn.softmax(logits)
+        prediction = tf.nn.softmax(logits, name='prediction')  # noqa
 
-        ## The next "cell" -- running the session  # noqa
-        num_steps = 1001
 
-        with tf.Session(graph=graph) as session:
-            tf.initialize_all_variables().run()
-            print('Initialized')
-            for step in range(num_steps):
-                offset = (step * batch_size) % (data['trainl'].shape[0] - batch_size)
-                batch_data = data['traind'][offset:(offset + batch_size), :, :, :]
-                batch_labels = data['trainl'][offset:(offset + batch_size), :]
-                feed_dict = {tf_dataset: batch_data, tf_labels: batch_labels}
-                _, l, predictions = session.run(
-                    [optimizer, loss, prediction], feed_dict=feed_dict)
-                if (step % 50 == 0):
-                    print('Minibatch loss at step %d: %f' % (step, l))
-                    print('Minibatch accuracy: %.1f%%' % accuracy(
-                        predictions, batch_labels))
-                    valid_predictions = session.run(
-                        prediction, feed_dict={tf_dataset: tf_valid_dataset.eval()})
-                    print('Validation accuracy: %.1f%%' % accuracy(
-                        valid_predictions, data['validl']))
-            test_predictions = session.run(
-                prediction, feed_dict={tf_dataset: tf_test_dataset.eval()})
-            print('Test accuracy: %.1f%%' % accuracy(
-                test_predictions, data['testl']))
+def train_graph(graph, data, params):
+    loss = graph.get_tensor_by_name('loss:0')
+    prediction = graph.get_tensor_by_name('prediction:0')
+    with graph.as_default():
+        # Optimizer.
+        optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+        init = tf.initialize_all_variables()
+
+    num_steps = 1001
+
+    session = tf.Session(graph=graph)
+    with session.as_default():
+        # tf.initialize_all_variables().run(session=session)
+        session.run(init)
+        print('Initialized')
+        for step in range(num_steps):
+            offset = ((step * params.batch_size) %
+                      (data['trainl'].shape[0] - params.batch_size))
+            batch_data = data['traind'][offset:(offset + params.batch_size), :, :, :]
+            batch_labels = data['trainl'][offset:(offset + params.batch_size), :]
+            feed_dict = {'tf_dataset:0': batch_data, 'tf_labels:0': batch_labels}
+            _, l, predictions = session.run(
+                [optimizer, loss, prediction], feed_dict=feed_dict)
+            if (step % 50 == 0):
+                print('Minibatch loss at step %d: %f' % (step, l))
+                print('Minibatch accuracy: %.1f%%' % accuracy(
+                    predictions, batch_labels))
+                valid_predictions = session.run(
+                    prediction, feed_dict={'tf_dataset:0': data['validd']})
+                print('Validation accuracy: %.1f%%' % accuracy(
+                    valid_predictions, data['validl']))
+        return session
+
+
+def test_model(session, data):
+    prediction = session.graph.get_tensor_by_name('prediction:0')
+    test_predictions = session.run(
+        prediction, feed_dict={'tf_dataset:0': data['testd']})
+    print('Test accuracy: %.1f%%' % accuracy(
+        test_predictions, data['testl']))
 
 
 def reformat(raw_data, data, prefix):
@@ -96,7 +108,15 @@ def main():
     raw_data, data = load_data(), {}
     for prefix in ['train', 'valid', 'test']:
         reformat(raw_data, data, prefix)
-    exercise_0(data)
+    params = namedtuple(
+        'Params', ['image_size', 'num_channels', 'num_labels',
+                   'batch_size', 'patch_size', 'depth', 'num_hidden']
+    )(28, 1, 10, 16, 5, 16, 64)
+
+    graph = tf.Graph()
+    create_graph_0(graph, params)
+    session = train_graph(graph, data, params)
+    test_model(session, data)
 
 
 if __name__ == '__main__':
