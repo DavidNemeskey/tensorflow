5b2e0b7bb7eab63c426a1d335b633266ca50513c
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-09-15 07:31:51 +0200

    Problem 1 solved.

--- tensorflow/examples/udacity/lstm_6.py
+++ tensorflow/examples/udacity/lstm_6.py
@@ -151,7 +151,7 @@ def random_distribution():
     return b/np.sum(b, 1)[:, None]
 
 
-def problem_1(batch_size, num_unrollings, num_nodes, iterations,
+def problem_0(batch_size, num_unrollings, num_nodes, iterations,
               train_batches, valid_batches, valid_size):
     graph = tf.Graph()
     with graph.as_default():
@@ -192,6 +192,147 @@ def problem_1(batch_size, num_unrollings, num_nodes, iterations,
             state = forget_gate * state + input_gate * tf.tanh(update)
             output_gate = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
             return output_gate * tf.tanh(state), state
+
+        # Input data.
+        train_data = list()
+        for _ in range(num_unrollings + 1):
+            train_data.append(
+                tf.placeholder(tf.float32, shape=[batch_size, VC.vocabulary_size]))
+        train_inputs = train_data[:num_unrollings]
+        train_labels = train_data[1:]  # labels are inputs shifted by one time step.
+
+        # Unrolled LSTM loop.
+        outputs = list()
+        output = saved_output
+        state = saved_state
+        for i in train_inputs:
+            output, state = lstm_cell(i, output, state)
+            outputs.append(output)
+
+        # State saving across unrollings.
+        with tf.control_dependencies([saved_output.assign(output),
+                                      saved_state.assign(state)]):
+            # Classifier.
+            logits = tf.nn.xw_plus_b(tf.concat(0, outputs), w, b)
+            loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(
+                    logits, tf.concat(0, train_labels)))
+
+        # Optimizer.
+        global_step = tf.Variable(0)
+        learning_rate = tf.train.exponential_decay(
+            10.0, global_step, 5000, 0.1, staircase=True)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        gradients, v = zip(*optimizer.compute_gradients(loss))
+        gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
+        optimizer = optimizer.apply_gradients(
+            zip(gradients, v), global_step=global_step)
+
+        # Predictions.
+        train_prediction = tf.nn.softmax(logits)
+
+        # Sampling and validation eval: batch 1, no unrolling.
+        sample_input = tf.placeholder(tf.float32, shape=[1, VC.vocabulary_size])
+        saved_sample_output = tf.Variable(tf.zeros([1, num_nodes]))
+        saved_sample_state = tf.Variable(tf.zeros([1, num_nodes]))
+        reset_sample_state = tf.group(
+            saved_sample_output.assign(tf.zeros([1, num_nodes])),
+            saved_sample_state.assign(tf.zeros([1, num_nodes])))
+        sample_output, sample_state = lstm_cell(
+            sample_input, saved_sample_output, saved_sample_state)
+        with tf.control_dependencies([saved_sample_output.assign(sample_output),
+                                      saved_sample_state.assign(sample_state)]):
+            sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
+
+    # Training
+    num_steps = 7001
+    summary_frequency = 100
+
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initialized')
+        mean_loss = 0
+        for step in range(num_steps):
+            batches = train_batches.next()
+            feed_dict = dict()
+            for i in range(num_unrollings + 1):
+                feed_dict[train_data[i]] = batches[i]
+            _, l, predictions, lr = session.run(
+                [optimizer, loss, train_prediction, learning_rate],
+                feed_dict=feed_dict)
+            mean_loss += l
+            if step % summary_frequency == 0:
+                if step > 0:
+                    mean_loss = mean_loss / summary_frequency
+                # The mean loss is an estimate of the loss over the last few batches.
+                print('Average loss at step %d: %f learning rate: %f' %
+                      (step, mean_loss, lr))
+                mean_loss = 0
+                labels = np.concatenate(list(batches)[1:])
+                print('Minibatch perplexity: %.2f' % float(
+                    np.exp(logprob(predictions, labels))))
+                if step % (summary_frequency * 10) == 0:
+                    # Generate some samples.
+                    print('=' * 80)
+                    for _ in range(5):
+                        feed = sample(random_distribution())
+                        sentence = characters(feed)[0]
+                        reset_sample_state.run()
+                        for _ in range(79):
+                            prediction = sample_prediction.eval({sample_input: feed})
+                            feed = sample(prediction)
+                            sentence += characters(feed)[0]
+                        print(sentence)
+                    print('=' * 80)
+
+                # Measure validation set perplexity.
+                reset_sample_state.run()
+                valid_logprob = 0
+                for _ in range(valid_size):
+                    b = valid_batches.next()
+                    predictions = sample_prediction.eval({sample_input: b[0]})
+                    valid_logprob = valid_logprob + logprob(predictions, b[1])
+                print('Validation set perplexity: %.2f' % float(np.exp(
+                    valid_logprob / valid_size)))
+
+
+def problem_1(batch_size, num_unrollings, num_nodes, iterations,
+              train_batches, valid_batches, valid_size):
+    """
+    You might have noticed that the definition of the LSTM cell involves 4
+    matrix multiplications with the input, and 4 matrix multiplications with the
+    output. Simplify the expression by using a single matrix multiply for each,
+    and variables that are 4 times larger."""
+    graph = tf.Graph()
+    with graph.as_default():
+        INPUT, FORGET, OUTPUT = range(3)
+        # Parameters:
+        # Common parameters for all gates
+        Gx = tf.Variable(tf.truncated_normal([VC.vocabulary_size, 4 * num_nodes], -0.1, 0.1))
+        Gm = tf.Variable(tf.truncated_normal([num_nodes, 4 * num_nodes], -0.1, 0.1))
+        Gb = tf.Variable(tf.zeros([1, 4 * num_nodes]))
+        # Variables saving state across unrollings.
+        saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        # Classifier weights and biases.
+        w = tf.Variable(tf.truncated_normal([num_nodes, VC.vocabulary_size], -0.1, 0.1))
+        b = tf.Variable(tf.zeros([VC.vocabulary_size]))
+
+        def wslice(tensor, begin=0, size=1):
+            return tf.slice(tensor, [0, begin * num_nodes], [-1, size * num_nodes])
+
+        # Definition of the cell computation.
+        def lstm_cell(i, o, state):
+            """
+            Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
+            Note that in this formulation, we omit the various connections between
+            the previous state and the gates.
+            """
+            gate_mult = tf.matmul(i, Gx) + tf.matmul(o, Gm) + Gb
+            gates = tf.sigmoid(wslice(gate_mult, size=3))
+            update = wslice(gate_mult, 2)
+            state = wslice(gates, FORGET) * state + wslice(gates, INPUT) * tf.tanh(update)
+            return wslice(gates, OUTPUT) * tf.tanh(state), state
 
         # Input data.
         train_data = list()
