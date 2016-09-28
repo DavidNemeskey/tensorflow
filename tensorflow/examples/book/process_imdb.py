52e5e040bec9168a1a1f5cce339670eea72a2baa
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-09-28 18:48:18 +0200

    Train - valid - test sets.

--- tensorflow/examples/book/process_imdb.py
+++ tensorflow/examples/book/process_imdb.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function
 from argparse import ArgumentParser
 from io import open
+import random
 import re
 import tarfile
 
@@ -17,11 +18,18 @@ def parse_arguments():
         description='Preprocesses the IMDB sentiment dataset.')
     parser.add_argument('input_file', help='the IMDB data (.tar) file.')
     parser.add_argument('output_prefix',
-                        help='the prefix of the output file. Two files are '
+                        help='the prefix of the output file. Four files are '
                              'written: a vocabulary file (.vocab) and the '
-                             'output file (.tsv).')
+                             'output files ({train,valid,test}.tsv).')
+    parser.add_argument('--valid-size', '-v', type=int, default=1000,
+                        help='the validation set size.')
+    parser.add_argument('--test-size', '-t', type=int, default=1000,
+                        help='the test set size.')
+    parser.add_argument('--random-seed', '-r', type=int, default=42,
+                        help='the random seed for shuffling the data.')
     args = parser.parse_args()
-    return args.input_file, args.output_prefix
+    return (args.input_file, args.output_prefix, args.valid_size,
+            args.test_size, args.random_seed)
 
 
 def read_tarfile(fn):
@@ -38,16 +46,29 @@ def _read(archive, filename):
         return [x.lower() for x in TOKEN_REGEX.findall(f.read().decode('utf-8'))]
 
 
-def main():
-    input_file, output_prefix = parse_arguments()
-    vocab = set()
-    with open(output_prefix + '.tsv', 'wt', encoding='utf-8') as outf:
-        for text, judgement in read_tarfile(input_file):
-            vocab.update(text)
+def write_file(fn, data):
+    with open(fn, 'wt', encoding='utf-8') as outf:
+        for text, judgement in data:
             print('{}\t{}'.format(judgement, ' '.join(text)), file=outf)
+
+
+def main():
+    input_file, output_prefix, valid_size, test_size, seed = parse_arguments()
+    data = list(read_tarfile(input_file))
+
+    # Write the vocabulary file
+    vocab = set()
+    for text, _ in data:
+        vocab.update(text)
     with open(output_prefix + '.vocab', 'wt', encoding='utf-8') as outf:
         for word in sorted(vocab):
             print(word, file=outf)
+
+    random.seed(seed)
+    random.shuffle(data)
+    write_file(output_prefix + '.train.tsv', data[:-(valid_size + test_size)])
+    write_file(output_prefix + '.valid.tsv', data[-(valid_size + test_size):-test_size])
+    write_file(output_prefix + '.test.tsv', data[-test_size:])
 
 
 if __name__ == '__main__':
