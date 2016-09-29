84bf1027f752d75fe430303974e29495ca85b758
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-09-29 13:15:22 +0200

    Renamed the file + added openall.

---
+++ tensorflow/examples/book/auxiliary.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""Auxiliary classes and methods."""
+import bz2
+import gzip
+
+class AttrDict(dict):
+    """Makes our life easier."""
+    def __getattr__(self, key):
+        if key not in self:
+            raise AttributeError
+        return self[key]
+
+    def __setattr__(self, key, value):
+        if key not in self:
+            raise AttributeError
+        self[key] = value
+
+
+# TODO Put this somewhere else
+def openall(
+    filename, mode='rt', encoding=None, errors=None, newline=None,
+    buffering=-1, closefd=True,  # , opener=None,  # for open()
+    compresslevel=5,  # faster default compression
+):
+    """
+    Opens all file types known to the Python SL. There are some differences
+    from the stock functions:
+    - the default mode is 'rt'
+    - the default compresslevel is 5, because e.g.gzip does not benefit a lot
+      from higher values, only becomes slower.
+    """
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode, compresslevel,
+                         encoding, errors, newline)
+    elif filename.endswith('.bz2'):
+        return bz2.open(filename, mode, compresslevel,
+                        encoding, errors, newline)
+    else:
+        return open(filename, mode, buffering, encoding, errors, newline,
+                    closefd)  # , opener)
