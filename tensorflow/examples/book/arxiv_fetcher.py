4fa095c380bbf2ebd146cd4be302815c8ec2968b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-10-03 11:13:21 +0200

    Fixed bugs, added a main() function.

--- tensorflow/examples/book/arxiv_fetcher.py
+++ tensorflow/examples/book/arxiv_fetcher.py
@@ -3,10 +3,11 @@
 
 """Fetches abstracts from ArXiv."""
 
+from argparse import ArgumentParser
 import os
 import random
 
-import BeautifulSoup
+import bs4
 import numpy as np
 import requests
 
@@ -24,13 +25,18 @@ class ArXivAbstracts(object):
             os.makedirs(cache_dir)
         filename = os.path.join(cache_dir, 'abstracts.txt')
         if not os.path.isfile(filename):
-            with open(filename, 'w') as file_:
-                for abstract in self._fetch_all(amount):
-                    file_.write(abstract + '\n')
+            try:
+                with open(filename, 'w') as file_:
+                    for abstract in self._fetch_all(amount):
+                        file_.write(abstract + '\n')
+            except:
+                if os.path.isfile(filename):
+                    os.remove(filename)
+                raise
         with open(filename) as file_:
-            self.data = file_.readlines()
+            self.data = [l.strip() for l in file_.readlines()]
 
-    def fetch_all(self, amount):
+    def _fetch_all(self, amount):
         """Implements pagination over SERPs."""
         page_size = type(self).PAGE_SIZE
         count = self._fetch_count()
@@ -43,7 +49,7 @@ class ArXivAbstracts(object):
     def _fetch_page(self, amount, offset):
         url = self._build_url(amount, offset)
         response = requests.get(url)
-        soup = BeautifulSoup(response.text)
+        soup = bs4.BeautifulSoup(response.text)
         for entry in soup.findAll('entry'):
             text = entry.find('summary').text
             text = text.strip().replace('\n', ' ')
@@ -52,7 +58,7 @@ class ArXivAbstracts(object):
     def _fetch_count(self):
         url = self._build_url(0, 0)
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
         count = int(soup.find('opensearch:totalresults').string)
         print(count, 'papers found')
         return count
@@ -91,9 +97,37 @@ class Preprocessing:
         for text in self.texts:
             for i in range(0, len(text) - self.length + 1, self.length // 2):
                 windows.append(text[i: i + self.length])
+                print(windows[-1])
         assert all(len(x) == len(windows[0]) for x in windows)
         while True:
             random.shuffle(windows)
             for i in range(0, len(windows), self.batch_size):
                 batch = windows[i: i + self.batch_size]
                 yield self(batch)
+
+
+def parse_arguments():
+    parser = ArgumentParser(
+        description='Fetches abstracts from ArXiv.')
+    parser.add_argument('--category', '-c', action='append', default=[],
+                        help='the categories (can specify more than once). ' +
+                             'Note that this must be the ArXiv designation, '
+                             'not meaningful words.')
+    parser.add_argument('--query', '-q', action='append', default=[],
+                        help='the query words (can specify more than once).')
+    parser.add_argument('--cache-dir', '-d', default='.',
+                        help='the cache directory [.].')
+    args = parser.parse_args()
+
+    if not len(args.query):
+        parser.error('At least one query word must be specified.')
+    return args.category, args.query, args.cache_dir
+
+
+def main():
+    category, query, cache_dir = parse_arguments()
+    ArXivAbstracts(cache_dir, category, query)
+
+
+if __name__ == '__main__':
+    main()
