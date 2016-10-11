a2cc2df3fe2f0927e054652a68e477bf4b1fd82f
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-10-11 10:13:35 +0200

    lstm_lm.py -> lstm_lm_old.py.

--- tensorflow/examples/book/lstm_lm_old.py
+++ tensorflow/examples/book/lstm_lm_old.py
@@ -18,10 +18,188 @@ import tensorflow as tf
 
 from auxiliary import AttrDict
 from file_reader import file_reader
-from lstm_model import LSTMModel
+
+BATCH, TIME, VOCAB = range(3)
 
 
-def get_sconfig(gpu_memory):
+class LSTMModel(object):
+    def __init__(self, params, is_training):
+        self.params = params
+        self.is_training = is_training
+
+        self.initial = None
+        self.data, self.target, self.mask, self.length = self._data()
+        self.initial_state, self.prediction, self.state = self._forward()
+        self.loss, self.error, self.logprob = \
+            self._cost(), self._error(), self._logprob()
+
+        # Optimization is not needed for the validation and testing cases
+        if is_training:
+            self.optimizer = tf.train.AdamOptimizer(self.params.learning_rate)
+            self.optimize = self._optimize()
+
+    def _data(self):
+        """
+        As before, we need the maximum length so that we know how much steps to
+        unroll.
+
+        This is where we introduce a temporal difference because at timestep t,
+        the model should have character s t as input but s t + 1 as target. As
+        easy way to obtain data or target is to slice the provided sequence and
+        cut away the last or the first frame, respectively.
+
+        We do this slicing using tf.slice() which takes the sequence to slice,
+        a tuple of start indices for each dimension, and a tuple of sizes for
+        each dimension. For the sizes -1 means to keep all elemnts from the
+        start index in that dimension until the end. Since we want to
+        slices frames, we only care about the second dimension.
+
+        We also define two properties on the target sequence as we already
+        discussed in earler sections: mask is a tensor of size batch_size x
+        num_unrolled where elements are zero or one depending on wheter the
+        respective frame is used or a padding frame. The length property sums
+        up the mask along the time axis in order to obtain the length of each
+        sequence.
+        """
+        num_unrolled = self.params.num_unrolled
+        self.sequence = tf.placeholder(
+            self.params.data_type, [None, num_unrolled, self.params.vocabulary])
+        data = tf.slice(self.sequence, (0, 0, 0), (-1, num_unrolled - 1, -1))
+        target = tf.slice(self.sequence, (0, 1, 0), (-1, -1, -1))
+        mask = tf.reduce_max(tf.abs(target), reduction_indices=VOCAB)
+        length = tf.reduce_sum(mask, reduction_indices=TIME)  # so /batch
+        return data, target, mask, length
+
+    def _forward(self):
+        """
+        The new part about the neural network code above is that we want to get
+        both the prediction and the last recurrent activation. Previously, we
+        only returned the prediction but the last activation allows us to
+        generate sequences more effectively later. Since we only want to
+        construct the graph for the recurrent network once, there is a forward
+        property that return the tuple of both tensors and prediction and state
+        are just there to provide easy access from the outside.
+        """
+        if self.params.embedding == 'yes':
+            with tf.device("/cpu:0"):
+                data = tf.argmax(self.data, VOCAB)
+                embedding = tf.get_variable(
+                    'embedding', [self.params.vocabulary, self.params.rnn_hidden],
+                    dtype=self.params.data_type)
+                self.inputs = tf.nn.embedding_lookup(embedding, data)
+        else:
+            self.inputs = self.data
+        if self.is_training and self.params.keep_prob < 1:
+            self.inputs = tf.nn.dropout(self.inputs, self.params.keep_prob)
+
+        cell = tf.nn.rnn_cell.BasicLSTMCell(self.params.rnn_hidden,
+                                            state_is_tuple=True)
+        if self.is_training and self.params.keep_prob < 1:
+            cell = tf.nn.rnn_cell.DropoutWrapper(
+                cell, output_keep_prob=self.params.keep_prob)
+        # TODO: max norm. reg.
+        if self.params.rnn_layers > 1:
+            cell = tf.nn.rnn_cell.MultiRNNCell([cell] * self.params.rnn_layers,
+                                               state_is_tuple=True)
+
+        initial_state = cell.zero_state(self.params.batch_size,
+                                        self.params.data_type)
+
+        hidden, state = tf.nn.dynamic_rnn(
+            inputs=self.inputs, cell=cell, dtype=self.params.data_type,
+            initial_state=initial_state, sequence_length=self.length)
+        vocabulary_size = int(self.target.get_shape()[2])
+        prediction = self._shared_softmax(hidden, vocabulary_size)
+        return initial_state, prediction, state
+
+    def _shared_softmax(self, data, out_size):
+        """Computes the shared softmax over all time-steps."""
+        num_unrolled = int(data.get_shape()[1])  # time-steps
+        in_size = int(data.get_shape()[2])     # (batch x time - 1 x >hidden<)
+        weight = tf.get_variable(
+            'softmax_w', [in_size, out_size], dtype=self.params.data_type,
+            initializer=tf.truncated_normal_initializer(
+                stddev=1 / math.sqrt(in_size)))
+        bias = tf.get_variable(
+            'softmax_b', shape=[out_size], dtype=self.params.data_type,
+            initializer=tf.constant_initializer(0.1))
+        # Flatten to apply same weights to all time steps.
+        flat = tf.reshape(data, [-1, in_size])
+        # Softmax just computes the values, doesn't choose the best, so we can
+        # just flatten and reshape the matrix
+        output = tf.nn.softmax(tf.matmul(flat, weight) + bias)
+        output = tf.reshape(output, [-1, num_unrolled, out_size])
+        return output
+
+    def _optimize(self):
+        gradient = self.optimizer.compute_gradients(self._cost())
+        if self.params.gradient_clipping:
+            limit = self.params.gradient_clipping
+            gradient = [
+                (tf.clip_by_value(g, -limit, limit), v)
+                if g is not None else (None, v)
+                for g, v in gradient]
+        optimize = self.optimizer.apply_gradients(gradient)
+        return optimize
+
+    def _cost(self):
+        """Cross-entropy loss."""
+        prediction = tf.clip_by_value(self.prediction, 1e-10, 1.0)
+        cost = self.target * tf.log(prediction)
+        cost = -tf.reduce_sum(cost, reduction_indices=VOCAB)
+        return self._average(cost)
+
+    def _error(self):
+        """This is basically inverse accuracy."""
+        error = tf.not_equal(
+            tf.argmax(self.prediction, VOCAB), tf.argmax(self.target, VOCAB))
+        error = tf.cast(error, self.params.data_type)
+        return self._average(error)
+
+    def _logprob(self):
+        """
+        The logprob property is new. It describes the probability that our
+        model assigned to the correct next character in logarithmic space. This
+        is basically the negative cross entropy transformed into logarithmic
+        space and averaged there. Converting the result back into linear space
+        yields the so-called perplexity, a common measure to evaluate the
+        performance of language models.
+
+        The perplexity can even become infinity when the model assigns a zero
+        probability to the next character once. We prevent this extreme case by
+        clamping the prediction probabilities within a very small positive
+        number and one.
+        """
+        logprob = tf.mul(self.prediction, self.target)
+        logprob = tf.reduce_max(logprob, reduction_indices=VOCAB)
+        logprob = tf.log(tf.clip_by_value(logprob, 1e-10, 1.0)) / tf.log(2.0)
+        return self._average(logprob)
+
+    def _average(self, data):
+        """
+        All the three properties above are averaged over the frames of all
+        sequences. With fixed-length sequences, this would be a single
+        tf.reduce_mean(), but as we work with variable-length sequences, we
+        have to be a bit more careful. First, we mask out padding frames by
+        multiplying with the mask. Then we aggregate along the frame size.
+        Because the Predictive coding three functions above all multiply with
+        the target, each frame has just one element set and we use
+        tf.reduce_sum() to aggregate each frame into a scalar.
+
+        Next, we want to average along the frames of each sequence using the
+        actual sequence length. To protect against division by zero in case of
+        empty sequences, we use the maximum of each sequence length and one.
+        Finally, we can use tf.reduce_mean() to average over the examples in
+        the batch.
+        """
+        data *= self.mask
+        length = tf.maximum(self.length, 1)  # To protect against / 0
+        # length = tf.reduce_sum(self.length, 0)  # To protect against / 0
+        data = tf.reduce_sum(data, reduction_indices=TIME) / length
+        return tf.reduce_mean(data)  # The avg. data / batch
+
+
+def _get_sconfig(gpu_memory):
     """
     Returns a session configuration object that sets the GPU memory limit.
     """
@@ -68,12 +246,12 @@ def parse_arguments():
                              'batches processed in an epoch.')
     parser.add_argument('--learning-rate', '-l', type=float, default=0.02,
                         help='the default learning rate [0.02].')
-    parser.add_argument('--max-grad-norm', '-g', type=float, default=None,
+    parser.add_argument('--gradient-clipping', '-g', type=float, default=None,
                         help='the limit for gradient clipping [None].')
     parser.add_argument('--early-stopping', type=int, default=0,
                         help='early stop after the perplexity has been '
                              'detoriating after this many steps. If 0 (the '
-                             'default), do not stop early.')
+                             'default), do not do early stopping.')
     parser.add_argument('--gpu-memory', type=float, default=None,
                         help='limit on the GPU memory ratio [None].')
     args = parser.parse_args()
@@ -203,7 +381,7 @@ def main():
 
     # TODO: look into Supervisor
     # The training itself
-    with tf.Session(graph=graph, config=get_sconfig(args.gpu_memory)) as sess:
+    with tf.Session(graph=graph, config=_get_sconfig(args.gpu_memory)) as sess:
         save_dir = os.path.join('saves', args.model_name)
         last_epoch = init_or_load_session(sess, save_dir, saver, init)
         print('Epoch {:2d}-                 valid PPL {:6.3f}'.format(
