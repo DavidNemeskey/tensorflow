43520e6ae215d3e6e31d1a52caff7470084dfd53
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-10-11 13:01:01 +0200

    Cont.

--- tensorflow/examples/book/cut_lm_input.py
+++ tensorflow/examples/book/cut_lm_input.py
@@ -7,6 +7,7 @@ the EoS </s> token.
 """
 
 from argparse import ArgumentParser
+import math
 import subprocess
 
 from auxiliary import openall
@@ -25,23 +26,46 @@ def parse_arguments():
 
 
 def input_length(input_files):
-    s = subprocess.check_output(['wc', '-wl'] + args.input_files)
+    s = subprocess.check_output(['wc', '-wl'] + input_files)
     fields = s.strip().rsplit('\n', 1)[1].strip().split()
     # field 3 is 'total' if |input_files| > 1 else the name of the file
     return int(fields[0]) + int(fields[1])
 
 
-def read_input(input_files):
-    vocab = {}
+def read_input(input_files, vocab):
+    """Reads the input files one-by-one and yields tokens line-by-line."""
     for input_file in input_files:
         with openall(input_file) as inf:
             for line in inf:
                 tokens = line.strip().split() + ['</s>']
+                yield tokens
+
+
+def divide_text(input_size, input_iter, batch_size, output_prefix):
+    """Does the work."""
+    num_outs = input_size // batch_size  # The modulo is dropped
+    out_size = input_size // num_outs
+    out_ext = '.{{:0{}}}'.format(int(math.ceil(math.log10(num_outs))))
+
+    with openall(output_prefix, 'wt') as header:
+        print('{}\t{}'.format(num_outs, input_size), file=header)
+    tokens = []
+    for outi in range(num_outs):
+        written = 0
+        with openall(output_prefix + out_ext.format(outi), 'wt') as outf:
+            tokens.extend(next(input_iter))
+            tokens_to_write = tokens[:out_size - written]
+            print(('\n'.join(tokens_to_write)), file=outf)
+            written += len(tokens_to_write)
+            tokens = tokens[len(tokens_to_write):]
+            if written == out_size:
+                continue
 
 
 def main():
     args = parse_arguments()
-    input_size = input_length(args.input_files)
+    vocab = {}
+    input_size = input_length(args.input_files, vocab)
 
 
 if __name__ == '__main__':
